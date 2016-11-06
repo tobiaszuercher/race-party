3e09c8d61aa88a6cfe35c86ecf973437f72e31c1
Tobias Zürcher <tobias.zuercher@gmail.com>
2016-11-06 13:49:44 +0100

    data gen

--- src/RaceParty.FlagMan.ProjectCars/Program.cs
+++ src/RaceParty.FlagMan.ProjectCars/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 using FluentScheduler;
 
-namespace ConsoleApplication
+using ServiceStack;
+
+namespace RaceParty.FlagMan.ProjectCars
 {
     public class Program
     {
@@ -10,31 +14,39 @@ namespace ConsoleApplication
         {
             Console.WriteLine("Starting Race Party");
             var registry = new Registry();
-            registry.Schedule<MyJob>().ToRunNow().AndEvery(2).Seconds();
-            registry.Schedule<MySecondJob>().ToRunNow().AndEvery(3).Seconds();
+            //registry.Schedule<RecordLapTimesOnButtonPressJob>().ToRunNow();
 
-            JobManager.Initialize(registry);
-            
-            Console.WriteLine("Press enter to stop");
-            Console.ReadLine();
+            //JobManager.Initialize(registry);
 
-            Environment.Exit(0);
-        }
-    }
+            var counter = 0;
+            var session = DateTime.Now.ToString("hh-mm-ss");
+        
+            while (true)
+            {
+                var input = Console.ReadKey();  
 
-    public class MyJob : IJob
-    {
-        public void Execute()
-        {
-            Console.WriteLine("gugus " + System.Threading.Thread.CurrentThread.ManagedThreadId);
-        }
-    }
+                if (input.Key == ConsoleKey.Spacebar)
+                {
+                    var filename = $"data/{session}_{counter:000}.json";
 
-    public class MySecondJob : IJob
-    {
-        public void Execute()
-        {
-            Console.WriteLine("zweiti job: " + System.Threading.Thread.CurrentThread.ManagedThreadId);
+                    var watch = new Stopwatch();
+                    watch.Start();
+                    File.WriteAllText(filename, "http://192.168.1.15:8080/crest/v1/api?timings=true&vehicleInformation=true&eventInformation=true".GetStringFromUrl());
+                    watch.Stop();
+
+                    Console.WriteLine($"Writing {filename} in {watch.ElapsedMilliseconds} ms.");
+
+                    //Console.WriteLine("gugus " + System.Threading.Thread.CurrentThread.ManagedThreadId);
+                }
+                else if (input.Key == ConsoleKey.Escape)
+                {
+                    Console.WriteLine("Finish!");
+
+                    Environment.Exit(0);
+                }
+
+                ++counter;
+            }
         }
     }
 }
