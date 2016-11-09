1aa414113c7c2ce7cc9870b5da37c28b0567634d
Tobias Zürcher <tobias.zuercher@gmail.com>
2016-11-09 09:25:42 +0100

    add dev logging

--- src/RaceParty/src/main.ts
+++ src/RaceParty/src/main.ts
@@ -1,5 +1,6 @@
 import {Aurelia} from 'aurelia-framework'
 import environment from './environment';
+import 'fetch';
 
 //Configure Bluebird Promises.
 //Note: You may want to use environment-specific configuration.
@@ -12,11 +13,8 @@ import environment from './environment';
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
+    .developmentLogging()
     .feature('resources');
-
-  if (environment.debug) {
-    aurelia.use.developmentLogging();
-  }
 
   if (environment.testing) {
     aurelia.use.plugin('aurelia-testing');
